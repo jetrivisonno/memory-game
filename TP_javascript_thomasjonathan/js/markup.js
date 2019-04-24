5a88613cb634504fe081724554d0ba76a892f2ab
jetrivisonno <jetrivisonno@gmail.com>
2019-04-24 12:32:32 -0400

    stuff

--- TP_javascript_thomasjonathan/js/markup.js
+++ TP_javascript_thomasjonathan/js/markup.js
@@ -63,12 +63,66 @@ const Markup = (function (){
             btnWrapper.append(btn.build())
         }
     }
+
+    class     Card {
+        const PAIR = 2
+        const NUMBER_OF_PAIRS = 9
+
+     createCardArray() {
+        let cardArray = []
+
+        for (let i = 0; i < NUMBER_OF_PAIRS; i++) {
+            for (let k = 0; k < PAIR; k++) {
+                let cardDiv = document.createElement('div')
+                let cardImg = document.createElement('img')
+
+                cardImg.src = 'images/' + i + '.jpeg'
+                
+                cardDiv.appendChild(cardImg)
+                cardDiv.classList.add('card')
+                cardArray.push(cardDiv)
+            }
+        }
+        
+        shuffleArray(cardArray)
+
+        return cardArray
+    }
+
+     shuffleArray(cardArray) {
+        let m = cardArray.length
+        let t
+        let i
+          
+        // While there remain elements to shuffle…
+        while (m) {
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+          
+            // And swap it with the current element.
+            t = cardArray[m];
+            cardArray[m] = cardArray[i];
+            cardArray[i] = t;
+        }
+    }
+
+     cardGenerator() {
+        const cardsContainer = document.getElementById('cardWrapper')
+        const cardArray = createCardArray()
+
+        for (let i = 0; i < cardArray.length; i++) {
+            cardsContainer.appendChild(cardArray[i])
+        }
+    }
+    }
+    
     return {
         init: function(GAME) {
             const wrap = wrapper()
             const container = frame()
             wrap.append(container)
             document.body.append(wrap)
+            Card.cardGenerator()
             build(GAME)
         }
     }
