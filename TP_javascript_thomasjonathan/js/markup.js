5ec8a61575b0e0e9ca267be1a1abf581edb2e1e0
tomkcey <tcaissie_dev@hotmail.com>
2019-04-24 12:32:02 -0400

    put the editor files in the right folder

--- TP_javascript_thomasjonathan/js/markup.js
+++ TP_javascript_thomasjonathan/js/markup.js
@@ -1,17 +1,17 @@
-const Markup = (function (){
+const Markup = (function () {
     'use strict'
     class Builder {
-        constructor(field) {
+        constructor (field) {
             this.field = field
         }
-        createElement(tagName, attributes) {
+        createElement (tagName, attributes) {
             const element = document.createElement(tagName)
-            for(const att in attributes) {
+            for (const att in attributes) {
                 element.setAttribute(attributes[att].name, attributes[att].value)
             }
             return element
         }
-        createInputElement(tagName, attributes) {
+        createInputElement (tagName, attributes) {
             const element = this.createElement(tagName, attributes)
             element.setAttribute('id', this.field.id)
             element.setAttribute('type', this.field.type)
@@ -21,9 +21,9 @@ const Markup = (function (){
         }
     }
     class StatisticBuilder extends Builder {
-        build() {
+        build () {
             const container = this.createElement('div', [])
-            const label = this.createElement('label', [{name: 'for', value: this.field.id}])
+            const label = this.createElement('label', [{ name: 'for', value: this.field.id }])
             const input = this.createInputElement('input', [])
             label.textContent = this.field.label
             input.disabled = true
@@ -33,40 +33,40 @@ const Markup = (function (){
         }
     }
     class CommandBuilder extends Builder {
-        build() {
+        build () {
             const btn = this.createInputElement('input', [])
             btn.removeAttribute('name')
             return btn
         }
     }
-    function wrapper() {
-        const container = new Builder().createElement('div', [{name:'wrapper', value:'wrapper'}])
+    function wrapper () {
+        const container = new Builder().createElement('div', [{ name: 'wrapper', value: 'wrapper' }])
         return container
     }
-    function frame() {
-        const container = new Builder().createElement('div', [{name:'id', value:'gameOptionsWrapper'}])
-        const statWrapper = new Builder().createElement('div', [{name:'id', value:'statWrapper'}])
-        const btnWrapper = new Builder().createElement('div', [{name:'id', value:'btnWrapper'}])
-        const cardWrapper = new Builder().createElement('div', [{name:'id', value:'cardWrapper'}])
+    function frame () {
+        const container = new Builder().createElement('div', [{ name: 'id', value: 'gameOptionsWrapper' }])
+        const statWrapper = new Builder().createElement('div', [{ name: 'id', value: 'statWrapper' }])
+        const btnWrapper = new Builder().createElement('div', [{ name: 'id', value: 'btnWrapper' }])
+        const cardWrapper = new Builder().createElement('div', [{ name: 'id', value: 'cardWrapper' }])
         container.append(statWrapper)
         container.append(btnWrapper)
         container.append(cardWrapper)
         return container
     }
-    function build(GAME) {
+    function build (GAME) {
         const statWrapper = document.getElementById('statWrapper')
         const btnWrapper = document.getElementById('btnWrapper')
-        for(const stat in GAME.statsFields) {
+        for (const stat in GAME.statsFields) {
             const row = new StatisticBuilder(GAME.statsFields[stat])
             statWrapper.append(row.build())
         }
-        for(const button in GAME.commandBtns) {
+        for (const button in GAME.commandBtns) {
             const btn = new CommandBuilder(GAME.commandBtns[button])
             btnWrapper.append(btn.build())
         }
     }
     return {
-        init: function(GAME) {
+        init: function (GAME) {
             const wrap = wrapper()
             const container = frame()
             wrap.append(container)
