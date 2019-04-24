3692b7d9b242f10dae4ad864d70f0040b98c4ce7
tomkcey <tcaissie_dev@hotmail.com>
2019-04-24 10:47:09 -0400

    made the gameoptions a module

--- TP_javascript_thomasjonathan/js/gameoptions.js
+++ TP_javascript_thomasjonathan/js/gameoptions.js
@@ -20,22 +20,18 @@ const GameOptions = (function (){
             return element
         }
     }
-    
     class StatisticBuilder extends Builder {
         build() {
             const container = this.createElement('div', [])
             const label = this.createElement('label', [{name: 'for', value: this.field.id}])
             const input = this.createInputElement('input', [])
             label.textContent = this.field.label
-            if(this.field.disabled) {
-                input.setAttribute('disabled', '')
-            }
+            input.disabled = true
             container.append(label)
             container.append(input)
             return container
         }
     }
-    
     class CommandBuilder extends Builder {
         build() {
             const btn = this.createInputElement('input', [])
@@ -43,9 +39,11 @@ const GameOptions = (function (){
             return btn
         }
     }
+    function frame() {}
+    function build() {}
     return {
         init: function() {
-            //
+            console.log('hello')
         }
     }
 })()
