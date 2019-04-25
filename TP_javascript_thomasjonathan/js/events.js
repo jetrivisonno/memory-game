f1e190f07a39ed5b9844d8991d71d5950a0a936a
tomkcey <tcaissie_dev@hotmail.com>
2019-04-25 08:08:34 -0400

    timer function done, but i find it looks dirty

--- TP_javascript_thomasjonathan/js/events.js
+++ TP_javascript_thomasjonathan/js/events.js
@@ -1,11 +1,43 @@
 const Events = (function () {
-    function start () {}
-    function stop () {}
-    function quit () {}
-    function flip () {}
+    let gameLoop = false
+
+    function start () {
+        detachEventListener('gameLoop', 'click', start)
+        attachEventListener('gameLoop', 'click', stop)
+        gameLoop = true
+        timer()
+    }
+    function stop () {
+        detachEventListener('gameLoop', 'click', stop)
+        attachEventListener('gameLoop', 'click', start)
+        gameLoop = false
+    }
+    // the timer function looks dirty!!!
+    function timer () {
+        if (gameLoop) {
+            setTimeout(function () {
+                if (gameLoop) {
+                    const timer = document.getElementById('timer')
+                    timer.value++
+                }
+                timer()
+            }, 1000)
+        }
+    }
+    function attachEventListener (id, event, feature) {
+        const element = document.getElementById(id)
+        element.addEventListener(event, feature)
+    }
+    function detachEventListener (id, event, feature) {
+        const element = document.getElementById(id)
+        element.removeEventListener(event, feature)
+    }
     return {
         init: function () {
-            console.log('event functions need to be coded!')
+            attachEventListener('quit', 'click', function () {
+                window.close()
+            })
+            attachEventListener('gameLoop', 'click', start)
         }
     }
 })()
