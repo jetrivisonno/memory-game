5933e426efbe9c1ecad4b49c4f6286469218ba5a
Jonathan Trivisonno <jtriviso@isimtl.local>
2019-04-26 12:54:00 -0400

    action listeners modification for cards

--- TP_javascript_thomasjonathan/js/events.js
+++ TP_javascript_thomasjonathan/js/events.js
@@ -9,7 +9,7 @@ const Events = (function () {
     let flippedCards = 0
     let flipDiv1 = null
     let flipDiv2 = null
-    let cardsMatched = 0
+    let pairsMade = 0
 
     function start (event) {
         detachEventListener('gameOn', 'click', start)
@@ -19,6 +19,7 @@ const Events = (function () {
         const target = event.target
         target.value = 'end game'
         timer()
+        attachEventListenerArray('card', 'click', flip)
     }
     function stop (event) {
         detachEventListener('gameOn', 'click', stop)
@@ -68,7 +69,6 @@ const Events = (function () {
     }
     function flipToggle (element) {
         element.classList.toggle('flip')
-        console.log('hello')
     }
     function flip (event) {
         const target = event.target
@@ -76,34 +76,45 @@ const Events = (function () {
         if (flippedCards < MAX_CARDS_FLIPPED) {
             flipDiv1 = target.parentNode.parentNode
             flipToggle(flipDiv1)
-            // flipDiv1.parentNode.removeEventListener('click', flip)
+            flipDiv1.removeEventListener('click', flip)
             pathFirstImg = target.nextSibling.firstChild.src
         } else {
             flipDiv2 = target.parentNode.parentNode
             flipToggle(flipDiv2)
-            flipDiv2.parentNode.removeEventListener('click', flip)
+            removeEventListenerArray('card', 'click', flip)
             compareCards(event)
             flippedCards = 0
         }
 
         if (discovered) {
             // keep cards discovered
-            cardsMatched++
-        } else {
-            setTimeout(function () {
-                flipToggle(flipDiv1)
-                flipDiv1.parentNode.addEventListener('click', flip)
-                flipToggle(flipDiv2)
-                flipDiv2.parentNode.addEventListener('click', flip)
-            }, 3000)
+            discovered = false
+            increasePair()
+            if (pairsMade === MAX_MATCHES) {
+                // win game functionality
+            }
         }
     }
     function compareCards (event) {
         const target = event.target
         if (pathFirstImg === target.nextSibling.firstChild.src) {
             discovered = true
+            flipDiv1.classList.add('discovered')
+            flipDiv2.classList.add('discovered')
+            attachEventListenerArray('card', 'click', flip)
+        } else {
+            setTimeout(function () {
+                flipToggle(flipDiv1)
+                flipToggle(flipDiv2)
+                attachEventListenerArray('card', 'click', flip)
+            }, 2000)
         }
         // check if there is any more cards to discover
+    }
+    function increasePair () {
+        const pairCounter = document.getElementById('pairs')
+        pairsMade++
+        pairCounter.value = pairsMade
     }
     function compareTime (time) {
         if (time > bestTime) {
@@ -129,7 +140,15 @@ const Events = (function () {
     function attachEventListenerArray (reference, event, feature) {
         const elementArray = document.getElementsByClassName(reference)
         for (let i = 0; i < elementArray.length; i++) {
-            elementArray[i].addEventListener(event, feature)
+            if (!elementArray[i].classList.contains('discovered')) {
+                elementArray[i].addEventListener(event, feature)
+            }
+        }
+    }
+    function removeEventListenerArray (reference, event, feature) {
+        const elementArray = document.getElementsByClassName(reference)
+        for (let i = 0; i < elementArray.length; i++) {
+            elementArray[i].removeEventListener(event, feature)
         }
     }
     return {
@@ -139,7 +158,6 @@ const Events = (function () {
             })
             attachEventListener('gameOn', 'click', start)
             attachEventListener('gameLoop', 'click', pause)
-            attachEventListenerArray('card', 'click', flip)
         }
     }
 })()
